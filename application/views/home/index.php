e94b31d866b641d215dfef71e19d143dd837bbc0
izza <izzalutfi045@gmail.com>
2020-05-29 04:07:47 +0700

    website desa hulosebo

---
+++ application/views/home/index.php
@@ -0,0 +1,470 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title><?=$title?></title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="manifest" href="site.webmanifest">
+	<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+	<!-- CSS here -->
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/ticker-style.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/flaticon.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/slicknav.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/animate.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/magnific-popup.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/themify-icons.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/slick.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/nice-select.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/home/css/style.css">
+</head>
+
+<body>
+
+	<!-- Preloader Start -->
+	<!-- <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/logo.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div> -->
+	<!-- Preloader Start -->
+
+	<header>
+		<!-- Header Start -->
+		<div class="header-area">
+			<div class="main-header ">
+				<div class="header-top black-bg d-none d-md-block">
+					<div class="container">
+						<div class="col-xl-12">
+							<div class="row d-flex justify-content-between align-items-center">
+								<div class="header-info-left">
+									<ul>
+										<li><img src="assets/img/icon/header_icon1.png" alt="">34ºc, Sunny </li>
+										<li><img src="assets/img/icon/header_icon1.png" alt="">Tuesday, 18th June, 2019
+										</li>
+									</ul>
+								</div>
+								<div class="header-info-right">
+									<ul class="header-social">
+										<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+										<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+										<li> <a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="header-mid d-none d-md-block">
+					<div class="container">
+						<div class="row d-flex align-items-center">
+							<!-- Logo -->
+							<div class="col-xl-3 col-lg-3 col-md-3">
+								<div class="logo">
+									<a href="index.html"><img src="<?=base_url()?>assets/image/logo1.png" width="280px"
+											height="80px" alt=""></a>
+								</div>
+							</div>
+							<div class="col-xl-9 col-lg-9 col-md-9">
+								<div class="header-banner f-right ">
+									<img src="<?=base_url()?>assets/home/img/hero/header_card.jpg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="header-bottom header-sticky">
+					<div class="container">
+						<div class="row align-items-center">
+							<div class="col-xl-10 col-lg-10 col-md-12 header-flex">
+								<!-- sticky -->
+								<div class="sticky-logo">
+									<a href="index.html"><img src="<?=base_url()?>assets/image/logo1.png" width="280px"
+											height="80px" alt=""></a>
+								</div>
+								<!-- Main-menu -->
+								<div class="main-menu d-none d-md-block">
+									<nav>
+										<ul id="navigation">
+											<li><a href="index.html">Home</a></li>
+											<li><a href="categori.html">Category</a></li>
+											<li><a href="about.html">About</a></li>
+											<li><a href="latest_news.html">Latest News</a></li>
+											<li><a href="contact.html">Contact</a></li>
+											<li><a href="#">Pages</a>
+												<ul class="submenu">
+													<li><a href="elements.html">Element</a></li>
+													<li><a href="blog.html">Blog</a></li>
+													<li><a href="single-blog.html">Blog Details</a></li>
+													<li><a href="details.html">Categori Details</a></li>
+												</ul>
+											</li>
+										</ul>
+									</nav>
+								</div>
+							</div>
+							<div class="col-xl-2 col-lg-2 col-md-4">
+								<div class="header-right-btn f-right d-none d-lg-block">
+									<i class="fas fa-search special-tag"></i>
+									<div class="search-box">
+										<form action="#">
+											<input type="text" placeholder="Search">
+
+										</form>
+									</div>
+								</div>
+							</div>
+							<!-- Mobile Menu -->
+							<div class="col-12">
+								<div class="mobile_menu d-block d-md-none"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Header End -->
+	</header>
+
+	<main>
+		<!-- Trending Area Start -->
+		<div class="trending-area fix">
+			<div class="container">
+				<div class="trending-main">
+					<!-- Trending Tittle -->
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="trending-tittle">
+								<strong>Selamat Datang!</strong>
+								<!-- <p>Rem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
+								<div class="trending-animated">
+									<ul id="js-news" class="js-hidden">
+										<li class="news-item">Selamat datang di website resmi Desa Hulosobo</li>
+										<li class="news-item">Silahkan manfaatkan fitur-fitur yang ada pada website ini
+										</li>
+									</ul>
+								</div>
+
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-8">
+							<!-- Trending Top -->
+							<div class="trending-top mb-30">
+								<div class="trend-top-img">
+									<img src="<?=base_url()?>assets/image/profil/profil.jpg" alt="">
+									<div class="trend-top-cap">
+										<span>Profil Desa</span>
+										<h2><a href="details.html">Profil Desa Hulosobo, Kaligesing, Purworejo, Jawa
+												Tengah</a></h2>
+									</div>
+								</div>
+							</div>
+							<!-- Trending Bottom -->
+							<div class="trending-bottom">
+								<div class="row">
+									<div class="col-lg-4">
+										<div class="single-bottom mb-35">
+											<div class="trend-bottom-img mb-30">
+												<img src="<?=base_url()?>assets/image/profil/misi.jpg" alt="">
+											</div>
+											<div class="trend-bottom-cap">
+												<span class="color1">Visi Misi</span>
+												<h4><a href="details.html">Visi dan Misi Desa Hulosobo</a></h4>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-4">
+										<div class="single-bottom mb-35">
+											<div class="trend-bottom-img mb-30">
+												<img src="<?=base_url()?>assets/image/profil/visi.jpg" alt="">
+											</div>
+											<div class="trend-bottom-cap">
+												<span class="color2">Pemerintahan Desa</span>
+												<h4>
+													<h4><a href="details.html">Susunan Organisasi dan Tata Kerja Desa
+															Hulosobo</a></h4>
+												</h4>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-4">
+										<div class="single-bottom mb-35">
+											<div class="trend-bottom-img mb-30">
+												<img src="<?=base_url()?>assets/image/profil/bpd.jpg" alt="">
+											</div>
+											<div class="trend-bottom-cap">
+												<span class="color3">BPD</span>
+												<h4><a href="details.html">Badan Musyawarah Desa Hulosobo</a>
+												</h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- Riht content -->
+						<div class="col-lg-4">
+							<div class="blog_right_sidebar">
+								<aside class="single_sidebar_widget search_widget">
+									<form action="#">
+										<div class="form-group">
+											<div class="input-group mb-3">
+												<input type="text" class="form-control" placeholder='Search Keyword'
+													onfocus="this.placeholder = ''"
+													onblur="this.placeholder = 'Search Keyword'">
+												<div class="input-group-append">
+													<button class="btns" type="button"><i
+															class="ti-search"></i></button>
+												</div>
+											</div>
+										</div>
+										<button class="button rounded-0 primary-bg text-white w-100 btn_1 boxed-btn"
+											type="submit">Search</button>
+									</form>
+								</aside>
+
+								<aside class="single_sidebar_widget post_category_widget">
+									<h4 class="widget_title">Category</h4>
+									<ul class="list cat-list">
+										<li>
+											<a href="#" class="d-flex">
+												<p>Resaurant food</p>
+												<p>(37)</p>
+											</a>
+										</li>
+										<li>
+											<a href="#" class="d-flex">
+												<p>Travel news</p>
+												<p>(10)</p>
+											</a>
+										</li>
+										<li>
+											<a href="#" class="d-flex">
+												<p>Modern technology</p>
+												<p>(03)</p>
+											</a>
+										</li>
+										<li>
+											<a href="#" class="d-flex">
+												<p>Product</p>
+												<p>(11)</p>
+											</a>
+										</li>
+										<li>
+											<a href="#" class="d-flex">
+												<p>Inspiration</p>
+												<p>21</p>
+											</a>
+										</li>
+										<li>
+											<a href="#" class="d-flex">
+												<p>Health Care (21)</p>
+												<p>09</p>
+											</a>
+										</li>
+									</ul>
+								</aside>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Trending Area End -->
+			<!--   Weekly2-News start -->
+			<div class="weekly2-news-area  weekly2-pading gray-bg">
+				<div class="container">
+					<div class="weekly2-wrapper">
+						<!-- section Tittle -->
+						<div class="row">
+							<div class="col-lg-12">
+								<div class="section-tittle mb-30">
+									<h3>Weekly Top News</h3>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-12">
+								<div class="weekly2-news-active dot-style d-flex dot-style">
+									<div class="weekly2-single">
+										<div class="weekly2-img">
+											<img src="<?=base_url()?>assets/home/img/news/weekly2News1.jpg" alt="">
+										</div>
+										<div class="weekly2-caption">
+											<span class="color1">Corporate</span>
+											<p>25 Jan 2020</p>
+											<h4><a href="#">Welcome To The Best Model Winner Contest</a></h4>
+										</div>
+									</div>
+									<div class="weekly2-single">
+										<div class="weekly2-img">
+											<img src="<?=base_url()?>assets/home/img/news/weekly2News2.jpg" alt="">
+										</div>
+										<div class="weekly2-caption">
+											<span class="color1">Event night</span>
+											<p>25 Jan 2020</p>
+											<h4><a href="#">Welcome To The Best Model Winner Contest</a></h4>
+										</div>
+									</div>
+									<div class="weekly2-single">
+										<div class="weekly2-img">
+											<img src="<?=base_url()?>assets/home/img/news/weekly2News3.jpg" alt="">
+										</div>
+										<div class="weekly2-caption">
+											<span class="color1">Corporate</span>
+											<p>25 Jan 2020</p>
+											<h4><a href="#">Welcome To The Best Model Winner Contest</a></h4>
+										</div>
+									</div>
+									<div class="weekly2-single">
+										<div class="weekly2-img">
+											<img src="<?=base_url()?>assets/home/img/news/weekly2News4.jpg" alt="">
+										</div>
+										<div class="weekly2-caption">
+											<span class="color1">Event time</span>
+											<p>25 Jan 2020</p>
+											<h4><a href="#">Welcome To The Best Model Winner Contest</a></h4>
+										</div>
+									</div>
+									<div class="weekly2-single">
+										<div class="weekly2-img">
+											<img src="<?=base_url()?>assets/home/img/news/weekly2News4.jpg" alt="">
+										</div>
+										<div class="weekly2-caption">
+											<span class="color1">Corporate</span>
+											<p>25 Jan 2020</p>
+											<h4><a href="#">Welcome To The Best Model Winner Contest</a></h4>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- End Weekly-News -->
+			<!--  Recent Articles start -->
+	</main>
+
+	<footer>
+		<!-- Footer Start-->
+		<div class="footer-area fix" style="padding-top:20px;">
+			<div class="container">
+				<div class="row d-flex justify-content-between">
+					<div class="col-xl-5 col-lg-5 col-md-7 col-sm-12">
+						<div class="single-footer-caption">
+							<div class="single-footer-caption">
+								<!-- logo -->
+								<div class="footer-logo">
+									<a href="index.html"><img src="<?=base_url()?>assets/image/logo1.png" width="280px"
+											height="80px" alt=""></a>
+								</div>
+								<div class="footer-tittle">
+									<div class="footer-pera">
+										<p>Suscipit mauris pede for con sectetuer sodales adipisci for cursus fames
+											lectus tempor da blandit gravida sodales Suscipit mauris pede for con
+											sectetuer sodales adipisci for cursus fames lectus tempor da blandit gravida
+											sodales Suscipit mauris pede for sectetuer.</p>
+									</div>
+								</div>
+								<!-- social -->
+								<div class="footer-social">
+									<a href="#"><i class="fab fa-twitter"></i></a>
+									<a href="#"><i class="fab fa-instagram"></i></a>
+									<a href="#"><i class="fab fa-pinterest-p"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-3 col-md-4  col-sm-6">
+						<div class="single-footer-caption mt-60">
+							<div class="footer-tittle">
+								<h4>Newsletter</h4>
+								<p>Heaven fruitful doesn't over les idays appear creeping</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-3 col-md-5 col-sm-6">
+						<div class="single-footer-caption mb-50 mt-60">
+							<div class="footer-tittle">
+								<h4>Instagram Feed</h4>
+							</div>
+							<div class="instagram-gellay">
+								<ul class="insta-feed">
+									<li><a href="#"><img src="<?=base_url()?>assets/home/img/post/instra1.jpg"
+												alt=""></a></li>
+									<li><a href="#"><img src="<?=base_url()?>assets/home/img/post/instra2.jpg"
+												alt=""></a></li>
+									<li><a href="#"><img src="<?=base_url()?>assets/home/img/post/instra3.jpg"
+												alt=""></a></li>
+									<li><a href="#"><img src="<?=base_url()?>assets/home/img/post/instra4.jpg"
+												alt=""></a></li>
+									<li><a href="#"><img src="<?=base_url()?>assets/home/img/post/instra5.jpg"
+												alt=""></a></li>
+									<li><a href="#"><img src="<?=base_url()?>assets/home/img/post/instra6.jpg"
+												alt=""></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- JS here -->
+
+	<!-- All JS Custom Plugins Link Here here -->
+	<script src="<?=base_url()?>assets/home/js/vendor/modernizr-3.5.0.min.js"></script>
+	<!-- Jquery, Popper, Bootstrap -->
+	<script src="<?=base_url()?>assets/home/js/vendor/jquery-1.12.4.min.js"></script>
+	<script src="<?=base_url()?>assets/home/js/popper.min.js"></script>
+	<script src="<?=base_url()?>assets/home/js/bootstrap.min.js"></script>
+	<!-- Jquery Mobile Menu -->
+	<script src="<?=base_url()?>assets/home/js/jquery.slicknav.min.js"></script>
+
+	<!-- Jquery Slick , Owl-Carousel Plugins -->
+	<script src="<?=base_url()?>assets/home/js/owl.carousel.min.js"></script>
+	<script src="<?=base_url()?>assets/home/js/slick.min.js"></script>
+	<!-- Date Picker -->
+	<script src="<?=base_url()?>assets/home/js/gijgo.min.js"></script>
+	<!-- One Page, Animated-HeadLin -->
+	<script src="<?=base_url()?>assets/home/js/wow.min.js"></script>
+	<script src="<?=base_url()?>assets/home/js/animated.headline.js"></script>
+	<script src="<?=base_url()?>assets/home/js/jquery.magnific-popup.js"></script>
+
+	<!-- Breaking New Pluging -->
+	<script src="<?=base_url()?>assets/home/js/jquery.ticker.js"></script>
+	<script src="<?=base_url()?>assets/home/js/site.js"></script>
+
+	<!-- Scrollup, nice-select, sticky -->
+	<script src="<?=base_url()?>assets/home/js/jquery.scrollUp.min.js"></script>
+	<script src="<?=base_url()?>assets/home/js/jquery.nice-select.min.js"></script>
+	<script src="<?=base_url()?>assets/home/js/jquery.sticky.js"></script>
+
+	<!-- contact js -->
+	<script src="<?=base_url()?>assets/home/js/contact.js"></script>
+	<script src="<?=base_url()?>assets/home/js/jquery.form.js"></script>
+	<script src="<?=base_url()?>assets/home/js/jquery.validate.min.js"></script>
+	<script src="<?=base_url()?>assets/home/js/mail-script.js"></script>
+	<script src="<?=base_url()?>assets/home/js/jquery.ajaxchimp.min.js"></script>
+
+	<!-- Jquery Plugins, main Jquery -->
+	<script src="<?=base_url()?>assets/home/js/plugins.js"></script>
+	<script src="<?=base_url()?>assets/home/js/main.js"></script>
+
+</body>
+
+</html>
