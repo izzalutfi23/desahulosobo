50a747fc6bf72ca6da0aba90c4ba7579e7530a32
izza <izzalutfi045@gmail.com>
2020-05-30 14:18:00 +0700

    menambahkan fitur export pdf

--- application/views/admin/_footer.php
+++ application/views/admin/_footer.php
@@ -28,19 +28,24 @@
 <!-- DataTables -->
 <script src="<?=base_url()?>assets/admin/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="<?=base_url()?>assets/admin/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
 <!-- page script -->
 <script>
-	$(function () {
-		$('#example1').DataTable()
-		$('#example2').DataTable({
-			'paging': true,
-			'lengthChange': false,
-			'searching': false,
-			'ordering': true,
-			'info': true,
-			'autoWidth': false
-		})
-	})
+	$(document).ready(function() {
+    $('#example1').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
 
 </script>
 </body>
